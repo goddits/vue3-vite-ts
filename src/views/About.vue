9893400858a78b83a79401fd463c47f30944d242
charleshong <qcsjin8@gmail.com>
2022-08-25 18:53:09 +0800

    vuex&ts配置

---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>about组件</div>
+</template>
+ 
+<script lang="ts">
+
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+  hello wolrd
+  <el-button>按钮</el-button>
+</div>
+</template>
+
+<script lang="ts">
+</script>
